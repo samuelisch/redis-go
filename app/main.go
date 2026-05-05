96196ded15cbe8d9d91c19e31c154015a04bace1
Samuel Chan <samuelchan@Samuels-MacBook-Pro.local>
2026-05-05 22:44:38 +0800

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -29,10 +29,12 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := listener.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handleConn(conn)
 	}
-	go handleConn(conn)
 }
