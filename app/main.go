49574d2a07d1e0af81d6da7014fb13f0df777085
Samuel Chan <samuelchan@Samuels-MacBook-Pro.local>
2026-05-07 17:52:27 +0800

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -94,7 +94,7 @@ func encodeArray(items []string) string {
 }
 
 func encodeNullArray() string {
-	return encodeBulkString("*-1\r\n")
+	return fmt.Sprintf("*-1\r\n")
 }
 
 func handlePing(args []string) string {
