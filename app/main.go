1d93ef800cf5b1efe914cfc211e66a9d9d4aa547
samuelisch <samuelchanwk@hotmail.com>
2026-05-16 16:18:02 +0800

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1078,7 +1078,7 @@ func startReplicationClient(masterHost string, masterPort string, ownPort string
 			conn.Close()
 			return fmt.Errorf("replication stream ended: %w", err)
 		}
-		replicaClient.replOffset += len(encodeArray(args))
+		replicaClient.replOffset = 0
 		cmd := strings.ToUpper(args[0])
 		if handler, ok := commandHandlers[cmd]; ok {
 			if resp := handler(replicaClient, args); resp != "" {
